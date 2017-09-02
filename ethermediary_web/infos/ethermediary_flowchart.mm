50b9009451fb8ede02c0cc83552da67b255c4402
Unknown <victor.deleau@gmx.com>
2017-09-02 18:11:21 +0200

    Improved UI + Mindmap

---
+++ ethermediary_web/infos/ethermediary_flowchart.mm
@@ -0,0 +1,137 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1504349788228" ID="ID_1757759036" MODIFIED="1504368077027">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The Buyer creates a new Deal
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1504352851735" HGAP="-15" ID="ID_279024882" MODIFIED="1504368231059" POSITION="right" TEXT="The Seller accept the Deal" VSHIFT="-99">
+<linktarget COLOR="#00ff05" DESTINATION="ID_279024882" ENDARROW="Default" ENDINCLINATION="-117;97;" ID="Arrow_ID_675730281" SOURCE="ID_1712786620" STARTARROW="None" STARTINCLINATION="-88;190;"/>
+<node CREATED="1504353524805" HGAP="230" ID="ID_1664812467" MODIFIED="1504368617381" TEXT="The Buyer send the right amount to the contract" VSHIFT="287">
+<node CREATED="1504353978951" HGAP="62" ID="ID_1184404379" MODIFIED="1504368070019" VSHIFT="-28">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - The contract send an email to the Buyer and Seller
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;- The Deal is accepted
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;- Here is the Deal information: amount, public address, etc
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;- Here is a received link http://ethermediary.com/blablabla
+    </p>
+    <p>
+      - The Seller send the object or activate the service.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1504367810065" HGAP="74" ID="ID_1588305018" MODIFIED="1504368006146" VSHIFT="32">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The Buyer and Seller have 6 month to finish or cancel the Deal
+    </p>
+  </body>
+</html>
+</richcontent>
+<linktarget COLOR="#00ff0d" DESTINATION="ID_1588305018" ENDARROW="Default" ENDINCLINATION="-158;175;" ID="Arrow_ID_720733574" SOURCE="ID_1032280541" STARTARROW="None" STARTINCLINATION="203;82;"/>
+<node CREATED="1504354430522" HGAP="110" ID="ID_283705921" MODIFIED="1504367897545" TEXT="The Buyer click the &quot;received&quot; link" VSHIFT="-74">
+<node CREATED="1504354566522" HGAP="58" ID="ID_1425144645" MODIFIED="1504355846704" VSHIFT="-6">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - The Seller is paid by the contract
+    </p>
+    <p>
+      - The Buyer gets his caution back
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1504354479698" HGAP="191" ID="ID_1912917035" MODIFIED="1504368464348" VSHIFT="3">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      One of them ask the other to cancel the Deal
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1504367724650" HGAP="81" ID="ID_730574012" MODIFIED="1504368107354" VSHIFT="4">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The other agree to cancel the Deal.
+    </p>
+    <p>
+      Everybody get his money back.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1504367778081" HGAP="85" ID="ID_1032280541" MODIFIED="1504368104842" VSHIFT="65">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The other refuse to cancel the Deal.
+    </p>
+  </body>
+</html>
+</richcontent>
+<arrowlink COLOR="#00ff0d" DESTINATION="ID_1588305018" ENDARROW="Default" ENDINCLINATION="-158;175;" ID="Arrow_ID_720733574" STARTARROW="None" STARTINCLINATION="203;82;"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1504368117402" HGAP="95" ID="ID_513621261" MODIFIED="1504368468116" TEXT="You guys fucked up, the Deal is canceled and the money can&apos;t be retrieved" VSHIFT="88">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1504353616922" HGAP="216" ID="ID_1957555060" MODIFIED="1504368612860" TEXT="The Buyer do not send the right amount to the contract" VSHIFT="-495">
+<node CREATED="1504353690341" HGAP="55" ID="ID_1712786620" MODIFIED="1504356141641" TEXT="The contract reject the transaction" VSHIFT="2">
+<arrowlink COLOR="#00ff05" DESTINATION="ID_279024882" ENDARROW="Default" ENDINCLINATION="-117;97;" ID="Arrow_ID_675730281" STARTARROW="None" STARTINCLINATION="-88;190;"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+<node CREATED="1504353102923" HGAP="-10" ID="ID_1263598254" MODIFIED="1504368246803" POSITION="right" TEXT="The Seller refuse the Deal" VSHIFT="96">
+<node CREATED="1504353141315" HGAP="62" ID="ID_1242015853" MODIFIED="1504355857576" TEXT="The Buyer is refund" VSHIFT="2">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</map>
